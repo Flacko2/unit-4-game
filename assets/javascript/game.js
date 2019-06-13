4c6ed522a96c4fede5121214a666b426e628e05f
Diego Matthews <diegomatthews9@gmail.com>
2019-06-12 22:49:48 -0700

    made a basic framework for javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,95 @@
-let randomNum = 0;
+let randomNum = 1;
 let count = 0;
 
 let gemOne;
 let gemTwo;
 let gemThree;
+let gemFour;
 
 let winCount;
-let lossCounter;
+let lossCount;
 
-//create random number and store it in my randomnum variable
-function gameStartup() {
-    function ranGenerator() {
-        randomNum = Math.floor(Math.random() * 35) + 100;
-        console.log(randomNum);
-        $("#ran-number").html(randomNum);
-    }
-    ranGenerator();
-    function gemGenerator() {
-        gemOne = Math.floor(Math.random() * 10) + 1;
-        console.log("gem 1 = " + gemOne);
-        gemTwo = Math.floor(Math.random() * 10) + 6;
-        if (gemTwo % 2 == 0){
-            gemTwo++;
-        }
-        console.log("gem 2 = " + gemTwo);
-        gemThree = Math.floor(Math.random() * 5) + 2;
-        console.log("gem 3 = " + gemThree);
-        
-    }
-    gemGenerator();
-//make random numbers for gems
-
+function ranGenerator() {
+    randomNum = Math.floor(Math.random() * 101) + 19;
+    console.log(randomNum);
+    $("#ran-number").html(randomNum);
 }
 
-gameStartup();
-
-
-
-//to do: create function that collects and  displays counter value
-
-if (randomNum == count){
- winCount++;
- $("#wins").html(winCount);
+function gemGenerator() {
+    gemOne = Math.floor(Math.random() * 12) + 1;
+    console.log("gem 1 = " + gemOne);
+    if (!(gemOne % 2 == 0)) {
+        gemOne++;
+    }
+    gemTwo = Math.floor(Math.random() * 12) + 1;
+    if (gemTwo % 2 == 0) {
+        gemTwo++;
+    }
+    console.log("gem 2 = " + gemTwo);
+    gemThree = Math.floor(Math.random() * 12) + 1;
+    console.log("gem 3 = " + gemThree);
+    gemFour = Math.floor(Math.random() * 12) + 1;
+    console.log("gem 4 = " + gemFour);
 }
-else {
+
+function clickGem() {
+
+
+    $("#gem1").on("click", function () {
+        count = count + gemOne;
+        console.log(count);
+        $("#counter").html(count);
+    });
+
+    $("#gem2").on("click", function () {
+        count = count + gemTwo;
+        console.log(count);
+        $("#counter").html(count);
+
+    });
+
+    $("#gem3").on("click", function () {
+        count = count + gemThree;
+        console.log(count);
+        $("#counter").html(count);
+    });
+
+    $("#gem4").on("click", function () {
+        count = count + gemFour;
+        console.log(count);
+        $("#counter").html(count);
+    });
+}
+
+function checkCount(count, randomNum) {
+    if (count === randomNum) {
+        console.log("it works bitch");
+    }
+    else if (count > randomNum) {
+        console.log("you suck!");
+    }
     
-$("#gem1").on("click", function(){
-    count = count + gemOne;
-    console.log(count);
-    $("#counter").html(count);
-})
-
-$("#gem2").on("click", function(){
-    count = count + gemTwo;
-    console.log(count);
-    $("#counter").html(count);
-})
-
-$("#gem3").on("click", function(){
-    count = count + gemThree;
-    console.log(count);
-    $("#counter").html(count);
-})
-
 }
+//create random number and store it in my randomnum variable
+function gameInit() {
+
+    ranGenerator();
+
+    gemGenerator();
+    clickGem();
+    //make random numbers for gems
+    //collect and display data for counter
+    checkCount();
+}
+
+
+gameInit();
+
+
 
 
 //to do: collect data for win or loss
+
+function resetGame() {
+
+} 
