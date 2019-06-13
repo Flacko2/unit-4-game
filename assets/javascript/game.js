81d8bdb4d5028f60537ccdd817a71f2f77c58311
Diego Matthews <diegomatthews9@gmail.com>
2019-06-12 18:26:22 -0700

    created random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+let randomNum;
+let count = 0;
+
+let gemOne;
+let gemTwo;
+let gemthree;
+
+let winCount;
+let lossCounter;
+
+//to do: create random number and store it in my randomnum variable
+function ranGenerator(){
+    randomNum = Math.floor(Math.random() * 35) + 100;
+    console.log(randomNum);
+}
+ranGenerator();
+
+//to do: make random numbers for gems
+
+//to do: create function that collects and  displays counter value
+
+//to do: collect data for win or loss
