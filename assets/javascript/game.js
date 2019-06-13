fc44a96b34331a9da3a6723f013b89ff6de0c31f
Diego Matthews <diegomatthews9@gmail.com>
2019-06-12 19:12:47 -0700

    created buttons that add to total

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,69 @@
-let randomNum;
+let randomNum = 0;
 let count = 0;
 
 let gemOne;
 let gemTwo;
-let gemthree;
+let gemThree;
 
 let winCount;
 let lossCounter;
 
-//to do: create random number and store it in my randomnum variable
-function ranGenerator(){
-    randomNum = Math.floor(Math.random() * 35) + 100;
-    console.log(randomNum);
-}
-ranGenerator();
+//create random number and store it in my randomnum variable
+function gameStartup() {
+    function ranGenerator() {
+        randomNum = Math.floor(Math.random() * 35) + 100;
+        console.log(randomNum);
+        $("#ran-number").html(randomNum);
+    }
+    ranGenerator();
+    function gemGenerator() {
+        gemOne = Math.floor(Math.random() * 10) + 1;
+        console.log("gem 1 = " + gemOne);
+        gemTwo = Math.floor(Math.random() * 10) + 6;
+        if (gemTwo % 2 == 0){
+            gemTwo++;
+        }
+        console.log("gem 2 = " + gemTwo);
+        gemThree = Math.floor(Math.random() * 5) + 2;
+        console.log("gem 3 = " + gemThree);
+        
+    }
+    gemGenerator();
+//make random numbers for gems
 
-//to do: make random numbers for gems
+}
+
+gameStartup();
+
+
 
 //to do: create function that collects and  displays counter value
+
+if (randomNum == count){
+ winCount++;
+ $("#wins").html(winCount);
+}
+else {
+    
+$("#gem1").on("click", function(){
+    count = count + gemOne;
+    console.log(count);
+    $("#counter").html(count);
+})
+
+$("#gem2").on("click", function(){
+    count = count + gemTwo;
+    console.log(count);
+    $("#counter").html(count);
+})
+
+$("#gem3").on("click", function(){
+    count = count + gemThree;
+    console.log(count);
+    $("#counter").html(count);
+})
+
+}
+
 
 //to do: collect data for win or loss
